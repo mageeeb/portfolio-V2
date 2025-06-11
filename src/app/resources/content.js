ec81ae04a069d7f055f6ea46b41b1d450b1d0104
Baye Fall <magibsall@gmail.com>
2025-06-11 16:50:27 +0200

    Gestion_blog_V4

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -120,7 +120,7 @@ const about = {
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
+            src: "/images/2.png",
             alt: "Once UI Project",
             width: 16,
             height: 9,
@@ -140,7 +140,7 @@ const about = {
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
+            src: "/images/2.png",
             alt: "Once UI Project",
             width: 16,
             height: 9,
